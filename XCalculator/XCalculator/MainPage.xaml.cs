55a6c1f94cbf023b6193acaeaf06450f9f57c06d
Saman-K <saman.kalanaki@yahoo.com>
2020-09-20 17:36:47 +0100

    First Upload

---
+++ XCalculator/XCalculator/MainPage.xaml.cs
@@ -0,0 +1,141 @@
+﻿using DCalc.Views;
+using System;
+using Xamarin.Forms;
+
+namespace DCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        string oprationPerformed;
+        Double? resultValue = null;
+        Double? lastresultValue = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (Editor.Text == "0" && button.Text == ".") { }
+            else if (Editor.Text == "0")
+            {
+                Editor.Text = "";
+            }
+
+            if (button.Text == ".")
+            {
+                if (!Editor.Text.Contains("."))
+                {
+                    Editor.Text = Editor.Text + button.Text;
+                }
+            }
+            else
+            {
+                Editor.Text = Editor.Text + button.Text;
+            }
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+
+            if (Editor.Text == "0" || Editor.Text == "" || Editor.Text == "." || Editor.Text == "0.")
+            {
+                oprationPerformed = button.Text;
+            }
+            else if ((resultValue == 0 || resultValue == null) && button.Text != "=" && (Editor.Text != "0" || Editor.Text != "" || Editor.Text != "." || Editor.Text != "0."))
+            {
+                resultValue = Double.Parse(Editor.Text);
+                oprationPerformed = button.Text;
+                Editor.Text = "0";
+            }
+            else if ((lastresultValue == 0 || lastresultValue == null) && (Editor.Text != "0" || Editor.Text != "" || Editor.Text != "." || Editor.Text != "0."))
+            {
+                switch (oprationPerformed)
+                {
+                    case "+":
+                        lastresultValue = (resultValue + Double.Parse(Editor.Text));
+                        break;
+                    case "-":
+                        lastresultValue = (resultValue - Double.Parse(Editor.Text));
+                        break;
+                    case "÷":
+                        lastresultValue = (resultValue / Double.Parse(Editor.Text));
+                        break;
+                    case "×":
+                        lastresultValue = (resultValue * Double.Parse(Editor.Text));
+                        break;
+                    default:
+                        break;
+                }
+                resultValue = Double.Parse(Editor.Text);
+                oprationPerformed = button.Text;
+                Editor.Text = "0";
+            }
+            else if ((lastresultValue != 0 || lastresultValue != null))
+            {
+                switch (oprationPerformed)
+                {
+                    case "+":
+                        lastresultValue = (lastresultValue + Double.Parse(Editor.Text));
+                        break;
+                    case "-":
+                        lastresultValue = (lastresultValue - Double.Parse(Editor.Text));
+                        break;
+                    case "÷":
+                        lastresultValue = (lastresultValue / Double.Parse(Editor.Text));
+                        break;
+                    case "×":
+                        lastresultValue = (lastresultValue * Double.Parse(Editor.Text));
+                        break;
+                    default:
+                        break;
+                }
+                resultValue = Double.Parse(Editor.Text);
+                oprationPerformed = button.Text;
+                Editor.Text = "0";
+            }
+
+            if (button.Text == "=")
+            {
+                Editor.Text = lastresultValue.ToString();
+                resultValue = null;
+                lastresultValue = null;
+                oprationPerformed = "";
+
+            }
+            else if (!(Editor.Text == "0" || Editor.Text == "" || Editor.Text == "." || Editor.Text == "0."))
+            {
+                oprationPerformed = button.Text;
+
+            }
+
+            LiveResultOprator.Text = resultValue + " " + oprationPerformed;
+            LiveResult.Text = lastresultValue.ToString();
+        }
+
+        private void BtnClearEntry_Click(object sender, EventArgs e)
+        {
+            Editor.Text = "0";
+            resultValue = null;
+            lastresultValue = null;
+            LiveResultOprator.Text = "";
+            LiveResult.Text = "";
+            oprationPerformed = "";
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            Editor.Text = "0";
+        }
+
+        private async void BtnAbout_ClickAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AboutPage());
+        }
+    }
+}
---
+++ XCalculator/XCalculator/Views/AboutPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCalc.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void BtnMain_ClickAsync(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
